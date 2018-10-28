3da4e832e738f2854351027f7e8825da0f0ed419
walmorp <walmorp@gmail.com>
2018-10-28 19:59:49 -0200

    Merge origin/master
    Conflicts:
    	Avisos1.php
    	Avisos2.php
    	composer.json
    	composer.lock
    	controller/Conexao.php
    	controller/ControleCadastro.php
    	controller/DaoBaixarParcela.php
    	controller/DaoCadastraParcelas.php
    	controller/DaoControleAssociado.php
    	controller/DaoControleCidade.php
    	controller/DaoControleCobranca.php
    	controller/DaoControleTipoCobranca.php
    	controller/DaoControleTitulo.php
    	controller/DaoHome.php
    	controller/DaoImprimirParcela.php
    	controller/DaoLoginAPP.php
    	controller/DaoRelacionaParcelasAssociado.php
    	controller/DaoRelatorioCobrador.php
    	controller/DaoRelatorioGerencial.php
    	controller/Estilo.css
    	controller/EstiloImp.css
    	controller/FuncaoSistema.php
    	controller/Scripts.js
    	controller/ValidaSessao.php
    	dados/BANCOASSOCIACAO.FDB
    	index.php
    	model/Associado.php
    	model/Cadastro.php
    	model/Cidade.php
    	model/Cobranca.php
    	model/Relatorio.php
    	model/Tabela.php
    	model/TipoCobranca.php
    	model/Titulo.php
    	model/View.php
    	nbproject/project.properties
    	view/AssociacaoAPP.php
    	view/BaixarParcela.php
    	view/BotoesCadastro.php
    	view/CadastraParcelas.php
    	view/ControleAssociado.php
    	view/ControleCidade.php
    	view/ControleCobranca.php
    	view/ControleTipoCobranca.php
    	view/ControleTitulo.php
    	view/DefineEstilo.php
    	view/DefineHeadMeta.php
    	view/DefineHeadMetaB.php
    	view/Home.php
    	view/ImprimirParcela.php
    	view/Login.php
    	view/LoginAPP.php
    	view/MensagemGravando.php
    	view/Mostra_Msg_Ret.php
    	view/RelacionaParcelasAssociado.php
    	view/RelatorioCobrador.php
    	view/RelatorioGerencial.php

--- aplicacao/controller/DaoBaixarParcela.php
+++ aplicacao/controller/DaoBaixarParcela.php
@@ -64,8 +64,6 @@ class DaoBaixarParcela extends Conexao implements View {
              WHERE ID = $id";
         $r = self::query($sql);
         
-        print "sql" .$sql;
-
         if (!$r) {
            print "Parcela não gravada";
            $gravou=false;
